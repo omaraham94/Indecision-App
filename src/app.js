e9bb8b8e2b0db5011c5cf03fbedd6b097976a7fa
Atul Golchha <Atul.Golchha@go-mmt.com>
2019-05-20 00:55:40 +0530

    working code after devtools config webpack done

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 class HelloWorld extends React.Component {
   render() {
-    consssole.log('hello world');
+    console.log('hello world');
     return (
       <div>
         <h1>Hello World</h1>
